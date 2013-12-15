f17c6c104d55f835b6667713d6188a41ecdf7bac
Sascha Riemer <sascha.riemer@gmail.com>
2013-12-15 19:15:07 +0100

    Erweiterung Generierung Einzelstimmen: Gesamter Wahlkreis

--- wis/dbFunctions.sql
+++ wis/dbFunctions.sql
@@ -13,6 +13,22 @@ begin
  end;
 $$ language plpgsql;
 
+create or replace function erzeugeErststimmen(pWahlkreisID int) returns void as $$
+declare
+  stimmsumme record;
+begin
+  alter table erststimme
+    drop constraint erststimme_kandidatid_fkey,
+    drop constraint erststimme_wahlkreisid_fkey;
+  for stimmsumme in select kandidatid from summeerststimmen where wahlkreisid = pWahlkreisID loop
+    perform erzeugeErststimmen(stimmsumme.kandidatid, pWahlkreisID);
+  end loop;
+  alter table erststimme
+    add constraint erststimme_kandidatid_fkey foreign key (kandidatid) references Kandidat (KandidatID),
+    add constraint erststimme_wahlkreisid_fkey foreign key (wahlkreisid) references Wahlkreis (WahlkreisID);
+end;
+$$ language plpgsql;
+
 create or replace function erzeugeErststimmen() returns void as $$
 declare
   stimmsumme record;
@@ -45,13 +61,39 @@ begin
  end;
 $$ language plpgsql;
 
+create or replace function erzeugeZweitstimmen(pWahlkreisID int) returns void as $$
+declare
+  stimmsumme record;
+begin
+  alter table zweitstimme
+    drop constraint zweitstimme_parteiid_fkey,
+    drop constraint zweitstimme_wahlkreisid_fkey;
+  drop index zweitstimme_parteiid_idx;
+  drop index zweitstimme_wahlkreisid_idx;
+  for stimmsumme in select parteiid from summezweitstimmen where wahlkreisid = pWahlkreisID loop
+    perform erzeugeZweitstimmen(stimmsumme.parteiid, pWahlkreisID);
+  end loop;
+  alter table zweitstimme
+    add constraint zweitstimme_parteiid_fkey foreign key (parteiid) references Partei (ParteiID),
+    add constraint zweitstimme_wahlkreisid_fkey foreign key (wahlkreisid) references Wahlkreis (WahlkreisID);
+  create index zweitstimme_parteiid_idx on zweitstimme using hash (parteiid);
+  create index zweitstimme_wahlkreisid_idx on zweitstimme using hash (wahlkreisid);
+end;
+$$ language plpgsql;
+
 create or replace function erzeugeZweitstimmen() returns void as $$
 declare
   stimmsumme record;
 begin
-    for stimmsumme in select parteiid,wahlkreisid from summezweitstimmen loop
-      perform erzeugeZweitstimmen(stimmsumme.parteiid, stimmsumme.wahlkreisid);
-    end loop;
+  alter table zweitstimme
+    drop constraint zweitstimme_parteiid_fkey,
+    drop constraint zweitstimme_wahlkreisid_fkey;
+  for stimmsumme in select parteiid,wahlkreisid from summezweitstimmen loop
+    perform erzeugeZweitstimmen(stimmsumme.parteiid, stimmsumme.wahlkreisid);
+  end loop;
+  alter table zweitstimme
+    add constraint zweitstimme_parteiid_fkey foreign key (parteiid) references Partei (ParteiID),
+    add constraint zweitstimme_wahlkreisid_fkey foreign key (wahlkreisid) references Wahlkreis (WahlkreisID);
 end;
 $$ language plpgsql;
 
